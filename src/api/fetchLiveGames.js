9f82ec45c0325d8df1ad9daa44bc84ef5eac54aa
Derrion-Malik Hall <dmalikhall17@gmail.com>
2022-11-10 21:22:31 -0500

    added env

--- src/api/fetchLiveGames.js
+++ src/api/fetchLiveGames.js
@@ -1,7 +1,7 @@
 export const liveOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '61f055d106msh0016cffd9a93878p153bacjsnd78fe1c5ff03',
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
 		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
 	}
 };
